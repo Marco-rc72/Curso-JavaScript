5d038a56fe5c6e80aac8e3c4cd3b168bd5bff15c
Marco-rc72 <marcocauichantonio@gmail.com>
2025-10-10 13:29:45 -0500

    docs

--- 04-blackjack-vite/src/blackjack/usecases/valor-carta.js
+++ 04-blackjack-vite/src/blackjack/usecases/valor-carta.js
@@ -7,7 +7,7 @@
  */
 
 
-export const valorCarta = ( carta ) => {
+export const valorCarta = ( carta) => {
 
     const valor = carta.substring(0, carta.length - 1);
     return ( isNaN( valor ) ) ? 
